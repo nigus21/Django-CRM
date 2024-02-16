f584678084f2345b4a07f2fe037917a3014efe92
unknown <dibekulunigus@gmail.com>
2024-02-16 11:25:09 +0300

    authentication

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    #checking if a person is logging in
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        #authnetication
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Welcome Back")
+            return redirect('home')
+        else:
+            messages.success(request,"Sorry, you must have mis-spelled your username or password")
+            return redirect('home')
+    else:
+        return render(request,'home.html', {})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"you have been logged out")
+    return redirect('home')
+
+def register_user(request):
+    return render(request,'register.html', {})
+
